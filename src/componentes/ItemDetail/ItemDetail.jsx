9ee95b65155a1ed14f864cf8c68bb828da12560f
pablot95 <pablo.travi95@gmail.com>
2024-09-18 13:42:45 -0300

    SegundaPreEntrega

--- src/componentes/ItemDetail/ItemDetail.jsx
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -11,7 +11,7 @@ const ItemDetail = ({id, nombre, precio, imagen}) => {
         <h2>{nombre} </h2>
         <p>${precio} </p>
         <p>ID: {id} </p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia, labore. Facilis commodi harum eligendi eum amet quia nesciunt asperiores. Quibusdam voluptates assumenda ipsam magni dignissimos totam ex culpa maiores atque?</p>
+        <p>No logro hacer que se vea cada producto segun el id</p>
 
 
     </div>
